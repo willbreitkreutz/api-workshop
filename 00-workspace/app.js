3b6eea4567fac420d9606946f7eab006b0da7264
Will Breitkreutz <will.l.breitkreutz@usace.army.mil>
2017-08-15 14:13:17 -0400

    Update app.js

--- 00-workspace/app.js
+++ 00-workspace/app.js
@@ -31,9 +31,12 @@ var app = {
   	typeAhead: function(e){
     	var el = e.target;
         var val = el.value;
-        app.queryAutocomplete(val, function(err, data){
-        	console.log(data);
-        })
+        
+      	if(val.length > 2){
+          app.queryAutocomplete(val, function(err, data){
+              console.log(data);
+          })
+        }
     },
   
   	queryAutocomplete: throttle(function(text, callback){
